3e5a2693a29f41aa5f580e4143d6876ab590639d
sidoncloud <133084113+sidoncloud@users.noreply.github.com>
2024-05-21 00:23:01 +0400

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,16 @@
+import json
+
+def lambda_handler(event, context):
+    # Print event to the CloudWatch logs
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    # Example processing
+    message = 'Hello from Lambda!'
+    
+    # Return a response
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': message
+        })
+    }
